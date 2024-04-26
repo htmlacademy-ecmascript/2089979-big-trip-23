76068654895847f8248f94e33d667e3470a23272
veronikali02 <veronikalikhacheva18@gmail.com>
2024-04-26 13:06:00 +0300

    Реализация presenter

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import ListFilterView from './view/list-filter-view.js';
+import PagePresenter from './presenter/page-presenter.js';
 import {render} from './render.js';
 
 const siteHeaderElement = document.querySelector('.trip-controls__filters');
+const siteMainElement = document.querySelector('.page-body__page-main');
+const pagePresenter = new PagePresenter({pageContainer: siteMainElement});
 
 render(new ListFilterView(), siteHeaderElement);
+pagePresenter.init();
